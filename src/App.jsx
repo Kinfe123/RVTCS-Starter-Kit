b9388f9a44be8afaf28f2667fa1a22416190b219
Kinfe123 <kinfishtech@gmail.com>
2023-05-04 23:50:27 +0300

    Editing and Polishing

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import tailwindcssLogo from './assets/tailwindcss.svg'
 import supabaseLogo from './assets/supabase.svg'
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { useUser } from '@clerk/clerk-react'
+import {AiFillGithub} from 'react-icons/ai'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const user = useUser()
+  
 
   return (
     <div>
@@ -28,17 +33,33 @@ function App() {
         </a>
       </div>
       <h1 className='b'>Vite + React + Supabase + Clerk + Tailwindcss</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='flex flex-row justify-center items-center my-10 gap-5'>
+        <button className='btn'>Go To Docs</button>
+       <a href="https://github.com/Kinfe123/RVTCS-Starter-Kit"> <button className='btn flex flex-row justify-center items-center gap-2'><AiFillGithub />Get the Github</button></a>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <div className="card">
+      <SignedIn>
+        <div className='absolute top-0 right-0 my-5 mx-5'>
+          
+        <UserButton afterSignOutUrl={window.location.href} />
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode='modal' />
+      </SignedOut>
+       
+      </div>
+     {user.isSignedIn && <p>Hellow {user.user && user.user.fullName} , 👩‍💻 Welcome back</p>}
+      {!user.isSignedIn && <p>Hello :) Unauthorized User. </p>}
+      <p className="read-the-docs my-4">
+        Click on the logos to learn more
       </p>
+
+      <footer className='absolute bottom-0 right-0'>
+        <p>Made with 😡<a href="https://bit.ly/KINFISHTECH" target='_blank' className='decoration-slice mx-1 my-1'>KIINFEMICHAEL TARIKU[KINFISH]</a></p>
+      </footer>
+     
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ClerkProvider, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+
+console.log(import.meta)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ClerkProvider publishableKey={import.meta.env.VITE_APP_CLERK_PUBLISHABLE_KEY}>
     <App />
-  </React.StrictMode>,
+  </ClerkProvider>,
 )
